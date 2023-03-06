545e051682dec0d44a1bee908407a47840d8b4bc
芜湖 <65854689+classic-takeoff@users.noreply.github.com>
2023-03-06 18:03:50 +0800

    new

---
+++ myVUE/MyVue/src/components/button.vue
@@ -0,0 +1,15 @@
+<script>
+   export default {
+    data() {
+        return {
+            title: 'My Journey with Vue'
+        }
+    },
+   }
+</script>
+
+<template>
+    <div>haha</div>
+</template>
+
+
